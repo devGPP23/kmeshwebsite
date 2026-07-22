acbca91bc41783408d5f9cea096f77bd4186e6d5
devGP7 <gauravpatil232005@gmail.com>
2026-07-22 18:44:32 +0530

    fix: add dark mode support and remove duplicate empty Link in Supporters component
    Signed-off-by: devGP7 <gauravpatil232005@gmail.com>

--- src/components/Supporters/index.js
+++ src/components/Supporters/index.js
@@ -3,18 +3,22 @@ import SectionContainer from "../sectionContainer";
 import Translate from "@docusaurus/Translate";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import ThemedImage from "@theme/ThemedImage";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 import "./index.scss";
 
 const supportList = [
   {
     name: "HuaweiCloud",
-    img_src: "img/supporters/huawei.png",
+    img_src: "img/supporters/huawei.svg",
+    img_src_dark: "img/supporters/huawei-dark.svg",
     external_link: "https://www.huaweicloud.com/",
   },
 
   {
     name: "OpenEuler",
     img_src: "img/supporters/openEuler.svg",
+    img_src_dark: "img/supporters/openEuler-dark.svg",
     external_link: "https://www.openeuler.org/",
   },
 ];
@@ -34,9 +38,14 @@ export default function Supporters() {
         {supportList.map((item, index) => (
           <div key={index} className="supporterBox">
             <div className="imgContainer">
-              <Link to={item.external_link}></Link>
               <Link to={item.external_link}>
-                <img alt={item.name} src={item.img_src} />
+                <ThemedImage
+                  alt={item.name}
+                  sources={{
+                    light: useBaseUrl(item.img_src),
+                    dark: useBaseUrl(item.img_src_dark || item.img_src),
+                  }}
+                />
               </Link>
             </div>
           </div>
